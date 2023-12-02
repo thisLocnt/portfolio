b8aae1c7c45f2d5cec2dc14a62f3f94a70e60008
locnt <ngtloc1401@gmail.com>
2023-12-03 03:50:21 +0700

    update Main có animation chạy chữ ---edit các link contact

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,64 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Link from 'next/link'
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { JOB_TITLE, FULL_NAME } from "../utils/constants";
 import { Tooltip } from "@material-tailwind/react";
 
+const textArray = ['Front-End Developer', 'Software Engineering'];
+
 const Main = () => {
+    const [roleText, setRoleText] = useState('')
+    const [start, setStart] = useState(0);
+    const [isNext, setIsNext] = useState(true);
+    const [delay, setDelay] = useState(50)
+
+    useEffect(() => {
+        setTimeout(() => {
+            const currentText = textArray[start]
+            if (isNext) {
+                if (roleText.length < currentText.length) {
+                    const split = currentText.split('')
+                    setRoleText(roleText + split[roleText.length])
+                } else {
+                    setIsNext(false)
+                    setDelay(500)
+                }
+            } else {
+                if (roleText.length !== 0) {
+                    setRoleText(roleText.slice(0, -1))
+                    setDelay(50)
+                } else {
+                    setIsNext(true)
+                    if (start === textArray.length - 1) {
+                        setStart(0)
+                    } else {
+                        setStart(start + 1)
+                    }
+                }
+            }
+        }, delay)
+    }, [roleText, isNext])
+
     return (
         <div className="w-full h-screen text-center">
-            <div 
-                className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center"
-            >
-            
-                <div 
-                // className="absolute w-screen h-screen top-16"
-                >
-                    {/* <p className="uppercase text-sm tracking-widest text-gray-500">
-                        Hi, I'm{' '}
-                    </p> */}
-                    <h1 className="py-4 text-gray-700 underline decoration-double decoration-slate-300">
-                        I am <span className="text-[#5651e5]">{FULL_NAME}</span>
+            <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+                <div>
+                    <h1 className="py-4 text-gray-700">
+                        👋 Hi, I'm{' '}<span className="text-[#5651e5]">{FULL_NAME}</span>
                     </h1>
-                    <h1 className="py-4 text-gray-700">A {JOB_TITLE}</h1>
+                    <h2 className="py-4 text-gray-700 underline decoration-single decoration-slate-300">
+                        {roleText}
+                        <span className='animate-[flicker_1s_infinite]'>_</span>
+                    </h2>
                     <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-                        I am highly motivated to create innovative and efficient applications, 
-                        and I am committed to keeping myself informed about the latest advancements and most effective methodologies 
-                        in the software engineering field.
+                        I'm just a programmer who enjoys learning and traveling.
                     </p>
                     <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-                        <Tooltip content="LinkedIn" placement="top" className="text-gray-600">
-                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
-                                <a
-                                    href={process.env.LINKEDIN_URL}
-                                    target='_blank'
-                                    rel='noreferrer'
-                                >
-                                    <FaLinkedinIn size={25} />
-                                </a>
-                            </div>
+                        <Tooltip content="LinkedIn" placement="bottom" className="text-gray-600">
+                            <a
+                                href={process.env.LINKEDIN_URL}
+                                target='_blank'
+                                rel='noreferrer'
+                                className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
+                            >
+                                <FaLinkedinIn size={25} />
+                            </a>
                         </Tooltip>
-                        <Tooltip content="Github" placement="top" className="text-gray-600">
-                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
-                                <a
-                                    href={process.env.GITHUB_URL}
-                                    target='_blank'
-                                    rel='noreferrer'
-                                >
-                                    <FaGithub size={25} />
-                                </a>
-                            </div>
+                        <Tooltip content="Github" placement="bottom" className="text-gray-600">
+                            <a
+                                href={process.env.GITHUB_URL}
+                                target='_blank'
+                                rel='noreferrer'
+                                className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
+                            >
+                                <FaGithub size={25} />
+                            </a>
                         </Tooltip>
-                        <Tooltip content="Email" placement="top" className="text-gray-600">
-                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                        <Tooltip content="Email" placement="bottom" className="text-gray-600">
+                            <a
+                                href={`mailto:${process.env.EMAIL}`}
+                                className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
+                            >
                                 <AiOutlineMail size={25} />
-                            </div>
+                            </a>
                         </Tooltip>
-                        <Tooltip content="Resume" placement="top" className="text-gray-600">
-                            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
-                                <BsFillPersonLinesFill size={25} />
-                            </div>
+                        <Tooltip content="Resume" placement="bottom" className="text-gray-600">
+                            <Link href='/api/download-cv'>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <BsFillPersonLinesFill size={25} />
+                                </div>
+                            </Link>
                         </Tooltip>
                     </div>
                 </div>
