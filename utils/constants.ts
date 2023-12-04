dcd50e006adb2a7a3ad6ad364603d3fcc4adad29
locnt <ngtloc1401@gmail.com>
2023-12-04 18:55:27 +0700

    update new image for iTest -- new projects constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,7 +7,7 @@ export const ERR_MSG = {
     required: 'Field is required'
 }
 
-export interface Skill{
+export interface Skill {
     thumbnail: string;
     name: string;
 }
@@ -43,7 +43,7 @@ export const SKILLS: Skill[] = [
     },
 ]
 
-interface ExperienceObject{
+interface ExperienceObject {
     name: string;
     content: string;
 }
@@ -65,11 +65,102 @@ export interface Project {
 export const PROJECTS: Project[] = [
     {
         projectId: 1,
+        projectName: 'i-Test',
+        thumbnail: '/assets/projects/itest-01.png',
+        mainTech: ['Next.js', 'TypeScript', 'NextAuth', 'MySQL', 'ChartJS',],
+        overview: 'i-Test is an online test management platform built to assist teachers in creating and managing online exam questions with ease. Additionally, it enables teachers to efficiently control and track scores while monitoring student learning outcomes.',
+        technologies: ['Next.js', 'TypeScript', 'NextAuth', 'React Suite', 'Sass', 'Exceljs', 'Docx', 'MySQL', 'Prisma', '...'],
+        linkDemo: 'https://i-test.vn/',
+        linkRepo: '',
+        experience: '',
+        experienceObject: [
+            {
+                name: '- Next Auth',
+                content: 'Manage users and permissions, and decentralize authority between management, admin, and teacher roles'
+            },
+            {
+                name: '- React Suite, Sass',
+                content: `Implements UI for the question input, the web version of the exam, and the mobile student's exam game`
+            },
+            {
+                name: '- Excel js',
+                content: `Export and format user result's excel file`
+            },
+            {
+                name: '- Chart JS',
+                content: 'Described and represented statistical data of the admin and user using charts from React ChartJS'
+            },
+            {
+                name: '- React to Print, Docx',
+                content: 'Implement the function of exporting test questions in PDF and Word format by React to print and Docx'
+            },
+            {
+                name: '- MySQL, Prisma',
+                content: 'Manage data entry database, store question sets, exam questions, save scores, and student information after taking the test '
+            },
+        ],
+        showDescription: 'Short description for i-Test'
+    },
+    {
+        projectId: 2,
+        projectName: 'Animation Dashboard',
+        thumbnail: '/assets/projects/dashboard-animation.png',
+        mainTech: ['React.js', 'React Chart JS', 'Material UI'],
+        overview: 'Presenting an admin Dashboard Login page with animation',
+        technologies: ['React.js', 'React Chart JS', 'Material UI'],
+        linkDemo: 'https://animate-dashboard.vercel.app/',
+        linkRepo: 'https://github.com/LOCNg0114/animate-dashboard',
+        experience: '',
+        experienceObject: [
+            {
+                name: '- React Chart JS',
+                content: 'Described and represented data of the admin and user using charts '
+            },
+            {
+                name: '- Material UI',
+                content: 'Learn to use Material UI components and icons'
+            },
+        ],
+        showDescription: 'Short description for Animation Dashboard'
+    },
+    {
+        projectId: 3,
+        projectName: 'Netflix Clone',
+        thumbnail: '/assets/projects/netflix-clone.JPG',
+        mainTech: ['Next.js', 'TypeScript', 'TailwindCSS', 'Material UI', 'Firebase'],
+        overview: 'This is a NETFLIX Clone that specializes in Server Side Rendering using Next.js, creating responsive user interfaces through the use of Tailwind CSS, and incorporating Typescript.',
+        technologies: ['Next.js', 'TypeScript', 'TailwindCSS', 'Material UI Icon', 'React Player', 'NextAuth', 'Firebase'],
+        linkDemo: 'https://netflix-clone-locng0114.vercel.app/',
+        linkRepo: 'https://github.com/LOCNg0114/netflix-clone',
+        experience: '',
+        experienceObject: [
+            {
+                name: '- Firebase',
+                content: 'Email/Password Authentication inside of our App using Firebase Auth'
+            },
+            {
+                name: '- Context API',
+                content: 'Implement shared state across our entire App using Context'
+            },
+            {
+                name: '- Material UI',
+                content: 'Learn to use Material UI components and icons'
+            },
+            {
+                name: '- TailwindCSS:',
+                content: 'Practice with this utility-first framework'
+            },
+
+        ],
+        showDescription: 'Short description for Netflix Website Clone'
+    },
+    {
+        projectId: 4,
         projectName: 'MERN Movie Website',
         thumbnail: '/assets/projects/reactjs-movie-website.JPG',
-        mainTech: ['ReactJS', 'Mongoose', 'Material UI', ],
+        mainTech: ['ReactJS', 'Mongoose', 'Material UI',],
         overview: 'Fullstack Responsive React.js Movie Website with MERN stack and Themoviedb. Some features of this project: \n\r - Sign up / Sign in \n\r - Save movie to Favorite list (Remove favorite item)\n\r- Write review for movie (Delete review) \n\r- Search movies / tv series / people \n\r- Light / Dark theme',
-        technologies: ['ReactJS', 'Redux toolkit', 'Material UI', 'SwiperJS', 'Formik', 'Yup', 'Express rest api', 'Express validator ', 'JWT' ],
+        technologies: ['ReactJS', 'Redux toolkit', 'Material UI', 'SwiperJS', 'Formik', 'Yup', 'Express rest api', 'Express validator ', 'JWT'],
         linkDemo: '',
         linkRepo: 'https://github.com/LOCNg0114/MERN_Movie_Web_2023',
         experience: '',
@@ -103,15 +194,15 @@ export const PROJECTS: Project[] = [
         showDescription: 'Short description for Movie Website'
     },
     {
-        projectId: 2,
+        projectId: 5,
         projectName: 'Next.js Blog App',
         thumbnail: '/assets/projects/blog-app.jpg',
         mainTech: ['Next.js', 'TypeScript', 'TailwindCSS'],
         overview: 'This is a project developed for a personal blog, with static data and utilizing the getStaticProp method to render the information',
-        technologies: ['NextJS', 'TypeScript','Tailwind CSS', 'SWR', 'SwiperJS', 'Axios'],
+        technologies: ['NextJS', 'TypeScript', 'Tailwind CSS', 'SWR', 'SwiperJS', 'Axios'],
         linkDemo: 'https://nextjs-blog-app-keuc.vercel.app',
         linkRepo: 'https://github.com/LOCNg0114/Nextjs-Blog-App',
-        experience:'',
+        experience: '',
         experienceObject: [
             {
                 name: '- NextJS:',
@@ -129,38 +220,7 @@ export const PROJECTS: Project[] = [
         showDescription: 'Short description for Blog App'
     },
     {
-        projectId: 3,
-        projectName: 'Netflix Clone',
-        thumbnail: '/assets/projects/netflix-clone.JPG',
-        mainTech: ['Next.js', 'TypeScript', 'TailwindCSS', 'Material UI', 'Firebase'],
-        overview: 'This is a NETFLIX Clone that specializes in Server Side Rendering using Next.js, creating responsive user interfaces through the use of Tailwind CSS, and incorporating Typescript.',
-        technologies: ['Next.js', 'TypeScript', 'TailwindCSS', 'Material UI Icon', 'React Player', 'NextAuth', 'Firebase'],
-        linkDemo: 'https://netflix-clone-locng0114.vercel.app/',
-        linkRepo: 'https://github.com/LOCNg0114/netflix-clone',
-        experience:'',
-        experienceObject: [
-            {
-                name: '- Firebase',
-                content: 'Email/Password Authentication inside of our App using Firebase Auth'
-            },
-            {
-                name: '- Context API',
-                content:'Implement shared state across our entire App using Context'
-            },
-            {
-                name: '- Material UI',
-                content: 'Learn to use Material UI components and icons'
-            },
-            {
-                name: '- TailwindCSS:',
-                content: 'Practice with this utility-first framework'
-            },
-
-        ],
-        showDescription:'Short description for Netflix Website Clone'
-    },
-    {
-        projectId: 4,
+        projectId: 6,
         projectName: 'Nodemailer Email',
         thumbnail: '/assets/projects/nodemailer.png',
         mainTech: ['ReactJS', 'Express', 'Nodemailer'],
@@ -168,7 +228,7 @@ export const PROJECTS: Project[] = [
         technologies: ['ReactJS', 'Express', 'Nodemailer', 'Nodemon'],
         linkDemo: '',
         linkRepo: 'https://github.com/LOCNg0114/Nodemailer_Gmail',
-        experience:'',
+        experience: '',
         experienceObject: [
             {
                 name: '- NodeJS',
